0f7ea1caf4a77fa5883950727507c3b5f8b3ac31
Andreas Romeyke <andreas.romeyke@slub-dresden.de>
2014-06-17 11:30:20 +0000

    - fixed README.1st - added License information

--- java/org/slub/rosetta/dps/repository/plugin/storage/nfs/SLUBStoragePlugin.java
+++ java/org/slub/rosetta/dps/repository/plugin/storage/nfs/SLUBStoragePlugin.java
@@ -1,3 +1,23 @@
+/*
+
+2014 by Andreas Romeyke (SLUB Dresden)
+
+The code contributed by SLUB is licensed under apache 2.0 and based partially
+on NFS Storage Plugin, 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package org.slub.rosetta.dps.repository.plugin.storage.nfs;
 
 import java.io.File;
@@ -8,7 +28,6 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Iterator;
-
 import com.exlibris.core.infra.common.exceptions.logging.ExLogger;
 import com.exlibris.core.infra.common.util.IOUtil;
 import com.exlibris.core.sdk.storage.containers.StoredEntityMetaData;
